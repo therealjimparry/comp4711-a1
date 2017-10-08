fbca6d91601ab70a52e5662908031e5ab39a7a05
Underrateddev <rajyudhvir29@gmail.com>
2017-10-08 00:55:32 -0700

    Changed data structure for flight models to make flights view work

--- application/views/flightView.php
+++ application/views/flightView.php
@@ -1,7 +1,7 @@
 <div class="row">
     <div class="span4">
-       {flight}
-        {departureLocation}
-       {/flight}
+       {all_flights}
+            {departureLocation}
+       {/all_flights}
     </div>
 </div>
