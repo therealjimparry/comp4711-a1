ece9674596b962495083c59b1c1beda6c6eb6c4f
Underrateddev <rajyudhvir29@gmail.com>
2017-10-07 22:14:02 -0700

    Commented home controller

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -2,20 +2,14 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+/*
+	Home controller, uses data from many models (flights, and fleets)
+*/
 class Home extends Application
 {
 
 	/**
 	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/
-	 * 	- or -
-	 * 		http://example.com/welcome/index
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
 	public function index()
 	{
