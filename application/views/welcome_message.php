95b28291caaa77ae971246e3394b4c814faa0f5d
Jim Ko <jimkoexpress@hotmail.com>
2017-10-08 00:11:06 -0700

    made homepage view

---
+++ application/views/welcome_message.php
@@ -0,0 +1,22 @@
+<h1>Welcome to Albatros Airlines!</h1>
+<div id="body">
+    <ul class="links" style="display: flex; list-style-type: none; justify-content: space-around;">
+        <li><a href="">Home</a></li>
+        <li><a href="">Fleet</a></li>
+        <li><a href="">Flights</a></li>
+    </ul>
+
+    <h2>Number of planes: {no_planes}</h2>
+    <h2>Number of flights: {no_flights}</h2>
+
+    <div>
+        <h2> List of airports:
+            {airports}
+            <li>{base}</li>
+            <li>{dest1}</li>
+            <li>{dest2}</li>
+            <li>{dest3}</li>
+            {/airports}
+        </h2>
+    </div>
+</div>
