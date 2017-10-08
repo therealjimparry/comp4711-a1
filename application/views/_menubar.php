f64cc24a4f0099a519f6757f8d9b16860b3502df
Jim Ko <jimkoexpress@hotmail.com>
2017-10-08 14:57:54 -0700

    changed ssl_verifypeer to true and bootstrapped homepage

--- application/views/_menubar.php
+++ application/views/_menubar.php
@@ -1,4 +1,4 @@
-<ul class="nav">
+<ul class="nav navbar-nav">
     {menudata}
     <li><a href="{link}">{name}</a></li>
     {/menudata}
--- application/views/template.php
+++ application/views/template.php
@@ -1,23 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<title>{pagetitle}</title>
-        <meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <link rel="stylesheet" type="text/css" href="/assets/css/default.css"/>
-	</head>
-	<body>
-        <div class="navbar">
-            <div class="navbar-inner">
+<head>
+    <meta charset="utf-8">
+    <title>{pagetitle}</title>
+    <meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" type="text/css" href="/assets/css/default.css"/>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+<body>
+
+<div id="container">
+    <div class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-collapse collapse" id="navbar-main">
                 {menubar}
             </div>
         </div>
+    </div>
+    <h1>Welcome to Albatros Airlines</h1>
 
-        <div id="container">
-			{content}
-			<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. 
-				{ci_version}</p>
-        </div>
-	</body>
+    {content}
+    <p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds.
+        {ci_version}</p>
+</div>
+</body>
 </html>
