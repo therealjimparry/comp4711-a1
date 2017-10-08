4356a8335511cc6d0cec2d3916450ba608384ddb
Stephen Cheng <aw3someone@gmail.com>
2017-10-08 15:16:10 -0700

    updated links on fleet view

--- application/views/fleet.php
+++ application/views/fleet.php
@@ -1,7 +1,7 @@
 <div class="row">
     {fleets}
     <div class="span4">
-        <a href="show/{key}">{key} - {id}</a>
+        <a href="fleet/{key}">{key} - {id}</a>
     </div>
     {/fleets}
 </div>
